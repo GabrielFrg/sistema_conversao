1eb1f5a55ddddd280d2a1a792a1c28c2c7adc1ad
Gabriel <bielfraga9310@gmail.com>
2026-04-28 11:10:14 -0300

    versão 3

--- bibliotecaFuncoes.php
+++ bibliotecaFuncoes.php
@@ -2,23 +2,42 @@
 
 namespace geometria{
 
-    function Quadrado ($lado);{
+    function Quadrado ($lado){
     return $lado*$lado;
     }
-function Retangulo ($base,$altura);{
+function Retangulo ($base,$altura){
     return $base*$altura;
 }
-function Triangulo ($base,$altura);{
+function Triangulo ($base,$altura){
     return $base*$altura/2;
 }
-function Circulo ($raio);{
+function Circulo ($raio){
     return 3.14*$raio**2;
 }
-function Trapesio ($basemaior,$basemenor,$altura);{
+function Trapesio ($basemaior,$basemenor,$altura){
     return $basemaior*$basemenor*$altura/2;
 }
 
 
+}
+
+namespace saude{
+
+function caucularimc($altura,$peso){
+    return $peso/$altura**2;
+}
+function valorIdealAgua($peso){
+    return $peso*35;
+}
+function frequenciaCardiacaMaxima($idade){
+    return 207- $idade*0.7;
+}
+function converterLibrasParaQuilo($libras){
+    return $libras*0.4543592;
+}
+function calcularCaloriasBasais($peso,$altura,$idade, ){
+    return 655 + $peso*9.6 + $altura*1.7 + $idade*4.7;
+}
 }
 
 ?>
--- entradadados.php
+++ entradadados.php
@@ -13,4 +13,16 @@ echo "A area do Triangulo é: ", Triangulo(3,6);
 echo "A area do Circulo é: ", Circulo(8);
 echo "A area do Trapesio é: ", Trapesio(5,8,5);
 
+use function saude\caucularimc;
+use function saude\valorIdealAgua;
+use function saude\frequenciaCardiacaMaxima;
+use function saude\converterLibrasParaQuilo;
+use function saude\calcularCaloriasBasais;
+
+
+echo "O seu imc é de: ", caucularimc(1.70,60);
+echo "o seu consumo ideal de agua é de: ", valorIdealAgua(60);
+echo "A frequencia cardiaca maxima é de: ", frequenciaCardiacaMaxima(25);
+echo "A conversao de libras para kg é: ", converterLibrasParaQuilo(50);
+echo "As suas calorias diarias são: ", calcularCaloriasBasais(60,1.70,25);
 ?>
